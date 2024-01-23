ee7ee18e2870a29d853309d0cd04b520a839c1ac
Jose Barrios <josebarrios@pursuit.org>
2024-01-23 14:52:44 -0500

    checking footer changes

---
+++ src/about.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function About(){
+  return (
+    <div>
+      <section>
+        <h1>Marlon Regis</h1>
+        <p>
+            "Been to all continents of the world, except 2!"
+            <br />
+            <a href="https://github.com/MarlonPelau">https://github.com/MarlonPelau</a> 
+            </p>
+      </section>
+      <section>
+        <h1>Jose Barrios</h1>
+        <p>
+            "I've been studying astronomy since I was 7!"
+            <br />
+            <a href="https://github.com/josebarrios23"> 
+            https://github.com/josebarrios23
+            </a>
+            </p>
+      </section>
+      <section>
+        <h1>Harleigh McLellan</h1>
+        <p>
+            "I really wanna start making video games.."
+            <br />
+            <a href="https://github.com/hjmclellan">
+            https://github.com/hjmclellan
+            </a>
+            </p>
+      </section>
+    </div>
+  );
+};
